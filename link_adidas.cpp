9ca3f69ef966cfec5fd6b8fff67b31da1786b59a
NewFeels <newfeelsproductions@gmail.com>
2019-03-23 19:52:51 +0100

    link adidas fixed

--- link_adidas.cpp
+++ link_adidas.cpp
@@ -1,49 +1,104 @@
+
 #include <iostream>
 #include <fstream>
-#include <stdlib.h>
+#include <iomanip>
+#include <cstring>
 #include <windows.h>
+#include <string.h>
+
 using namespace std;
 
-
-const int MAX_LONG_WORD=20;
-const int MAX_LONG_FICHERO=120;
-
-
-bool asignar (const char nombre_fichero[], char ropas[],char color[]){
-    bool abierto = false;
-    ifstream f;
-    f.open(nombre_fichero);
+const int MAX_LONG= 200;
+const int NUM_CLOTHES=13;
+const int NUM_COL=15;
+const char PRENDAS[NUM_CLOTHES][MAX_LONG]={ //prendas que se pueden procesar
+    "pants","trainers","shoes","trousers","t-shirt", "shorts","short", "jacket","backpack",
+    "ball","gloves","football","sunglasses","socks","sweatshirts","tights","dresse","dresses"
+    "skirt"
+};
+const char COLORS[NUM_COL][MAX_LONG]={//colores de ropa
+        "blue","grey","black","white","red","pink","green","multicolour",
+        "orange","purple","turquoise","yellow","beige","brown","gold"
+};
+const char INICIO_URL[MAX_LONG]="start https://www.adidas.co.uk/search?q=";
+const char PRECOLOR[MAX_LONG]="%20";
+//
+bool encontrarPrenda(const char nombreFichero[],char prenda[],char color[],bool& estaColor){
+    ifstream f(nombreFichero);
+    bool prendaEncontrada = false;
+    estaColor=false;
     if (f.is_open()){
-        f >> ropas;
-        if (!f.eof()){
-            f.ignore(15, '\n');
-            f >> color;
-            if (!f.eof()){
-                abierto = true;
+        char prendaLeida[MAX_LONG];
+        
+
+        f.getline(prendaLeida,MAX_LONG,'\n');
+        cout<<"primera lectura";
+        while(!f.eof()){
+            cout<<prendaLeida<<endl; 
+            if (!prendaEncontrada){
+                
+               for(int i =0; i < NUM_CLOTHES;i++){
+                     if(strcmp(PRENDAS[i],prendaLeida)==0&&!prendaEncontrada){
+                        prendaEncontrada=true;
+                        strcpy(prenda,prendaLeida);
+                        cout<<"encontrada prenda"<<endl;
+                    }
+                }
+                
             }
+            else{
+                cout<<"buscamos color"<<endl;
+                for(int i = 0; i<NUM_COL; i++){
+                     
+                    if (strcmp(COLORS[i],prendaLeida)==0){
+                        strcpy(color,prendaLeida);
+                        estaColor=true;
+                        return true;
+                    }
+                }
+            }
+            
+            f>>prendaLeida;
+            
+            
         }
-    }
-    f.close();
-    return abierto;
-}
-
-
-int main (){
-    char primera[MAX_LONG_FICHERO]="start https://www.adidas.co.uk/search?q=";
-    char color[MAX_LONG_WORD];char ropa[MAX_LONG_WORD];
-    char tercera[MAX_LONG_FICHERO]="%20";
-
-    char nombre_fichero[MAX_LONG_FICHERO]="ropas.txt";
-    if (asignar(nombre_fichero,ropa,color)){
-         strcat(primera,ropa);
-         strcat(tercera, color);
-         strcat(primera,tercera);
-         cout << primera << endl;
-         system(primera);
-         system("pause");
+        f.close();
+        
     }
     else{
-        cerr << "something bad has happenned" << endl;
+        cerr<< "no se abre "<< nombreFichero<<endl;
+    }
+
+    return prendaEncontrada;
+}
+
+int main(int numArg, char* args[]){
+    char etiquetasFoto[MAX_LONG];
+    strcpy(etiquetasFoto,args[1]);
+    cout<<etiquetasFoto<<endl;
+    char prenda [MAX_LONG];
+    char color[MAX_LONG];
+    bool estaColor;
+    if (encontrarPrenda(etiquetasFoto, prenda, color,estaColor)){
+        char link [MAX_LONG];
+        cout<<  prenda;
+
+        strcpy(link,INICIO_URL);
+        strcat(link,prenda);
+        if(estaColor){
+            cout << color ;
+            strcat(link,PRECOLOR);
+            strcat(link,color);
+        }
+        system(link);
+        system("pause");
+        
+    }
+    else{
+        cerr << "No se pudo abrir el archivo " << etiquetasFoto << endl;
+
     }
     return 0;
 }
+
+
