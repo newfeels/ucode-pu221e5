6cd8311f3d117b8fc14d0698ab87c9268e54ae0a
NewFeels <newfeelsproductions@gmail.com>
2019-03-23 02:14:17 +0100

    esta movida escribe el link de adidas en c++

---
+++ link_adidas.cpp
@@ -0,0 +1,40 @@
+//BORRADOR
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+const int MAX_LONG_WORD=15;
+const int MAX_LONG_FICHERO=30;
+struct ropa {
+    char clothes[MAX_LONG_WORD];
+    char colours[MAX_LONG_WORD];
+    
+};
+
+bool asignar (char nombre_fichero[], ropa ropas){
+    bool abierto = false;
+    ifstream f;
+    f.open(nombre_fichero);
+    if (f.is_open()){
+        f >> ropas.clothes;
+        if (!f.eof()){
+            f.ignore(15, '\n');
+            f >> ropas.colours;
+            if (!f.eof()){
+                abierto = true;
+        }
+    }
+    return abierto;
+    f.close;
+
+}
+int main (){
+    int nombre_fichero[MAX_LONG_FICHERO]="";
+    if (asignar(nombre_fichero,ropa ropas)==true){
+        cout << "https://www.adidas.co.uk/search?q=" << ropas.clothes << "&searchclour=" << ropas.colours << endl; 
+    }
+    else{
+        cerr << "something bad has happenned" << endl;
+    }
+    return 0;
+}
