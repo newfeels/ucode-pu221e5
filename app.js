e8ce2f1d9f4425a90a58ce5ab3a35556f5711c5c
Jose <778148@unizar.es>
2019-03-24 03:45:55 +0100

    Node funciona

--- app.js
+++ app.js
@@ -8,14 +8,14 @@ const s3 = new AWS.S3();
 var rekognition = new AWS.Rekognition();
 // ejecutar comandos y abrir enlace
 var exec = require('child_process').exec;
-const command = "./cpp/procesador";
+const command = './procesador';
 var opn = require('opn');
 
 const image = {
  Image: {
   S3Object: {
    Bucket: "ucode19",
-   Name: "teams/group-20/celebrities.jpg"
+   Name: "teams/group-20/shirt.jpg"
   }
  },
  MaxLabels: 20,
@@ -23,35 +23,33 @@ const image = {
 };
 
 // APP DE NODE BASICA
-/*
 http.createServer(function (req, res) {
   if (req.url == '/upload') {
     var form = new formidable.IncomingForm();
     form.parse(req, function (err, fields, files) {
+      res.write('Cargando...');
       loadFileToS3();
       res.end();
     });
   } else {
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
+    res.write('<form action="upload" method="post" enctype="multipart/form-data">');
     res.write('<input type="file" name="filetoupload"><br>');
     res.write('<input type="submit">');
     res.write('</form>');
     return res.end();
   }
 }).listen(8080);
-*/
 
 // APP DE NODE CON CSS
+/*
 http.createServer(function(req, res) {
   if (req.url != 'upload') {
-    /*
-    fs.readFile('index.html', function(err, data) {
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      res.write(data);
-      return res.end();
-    });
-    */
+    //fs.readFile('index.html', function(err, data) {
+    //  res.writeHead(200, {'Content-Type': 'text/html'});
+    //  res.write(data);
+    //  return res.end();
+    //});
     if (req.url == '/upload') {
       // HTML
       res.writeHead(200, {'Content-Type':'text/html'});
@@ -71,29 +69,44 @@ http.createServer(function(req, res) {
     }
   }
 }).listen(8080);
-
+*/
 
 // PROCESO DE LOS DATOS
 var objEtiquetas;
+
 
 function procesarDatos() {
   // escribir el archivo
   let testStr = "";
   for (let i=0; i<objEtiquetas.Labels.length;  i++) {
-    testStr += objEtiquetas.Labels[i].Name + "\n";
+    testStr += objEtiquetas.Labels[i].Name.toLowerCase() + "\n";
   }
-  fs.writeFile('cpp/datos.txt', testStr);
-  // ejecutar c++
-  exec(command, function callback(error, stdout, stderr){
-      console.log(error, stdout, stderr);
-  });
-
-  fs.readFile('cpp/url.txt', function(err, data) {
+  fs.writeFile('datos.txt', testStr, function(err) {
     if (err) {
-      console.log("Error al abrir el archivo con el enlace");
-    } else {
-      console.log("Redireccionando a ", data);
-      opn(data);
+      console.log("Error al abrir datos.txt:", err);
+    }
+    else {
+      // ejecutar c++
+      exec(command, function callback(error, stdout, stderr) {
+        if (error) {
+          console.log("Error:", error, "| stdout: ", stdout, "| stderr: ", stderr);
+        }
+        else {
+          // Abrir enlace
+          fs.readFile('url.txt', 'utf8', function(err, data) {
+            if (err) {
+              console.log("Error al abrir el archivo con el enlace");
+            }
+            else if (data == null || data == "") {
+              console.log("No se encontraron etiquetas");
+            }
+            else {
+              console.log("Redireccionando a", data);
+              opn(data.toString());
+            }
+          });
+        }
+      });
     }
   });
 }
--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ using namespace std;
 
 const int MAX_LONG= 200;                            //numero maximo de caracteres
 const int NUM_CLOTHES=25;                           // tamaño de la base de datos de prendas
-const int NUM_COL=15;                               // tamaño de la base de datos de colores   
+const int NUM_COL=15;                               // tamaño de la base de datos de colores
 const char PRENDAS[NUM_CLOTHES][MAX_LONG]={         //prendas que se pueden procesar
     "pants","trainers","shoes","trousers","t-shirt", "shorts","short", "jacket","backpack",
     "ball","gloves","football","sunglasses","socks","sweatshirts","tights","dresse","dresses",
@@ -18,16 +18,16 @@ const char COLORS[NUM_COL][MAX_LONG]={              //colores de ropa
         "blue","grey","black","white","red","pink","green","multicolour",
         "orange","purple","turquoise","yellow","beige","brown","gold"
 };
-const char INICIO_URL[MAX_LONG]="https://www.adidas.co.uk/";//pagina principal de adidas
+const char INICIO_URL[MAX_LONG]="https://www.adidas.co.uk/search?q=";//pagina principal de adidas
 const char PRECOLOR[MAX_LONG]="%20";                //codigo de colores
 const char POSTGEN[3]="-";
 const char GENDER[2][MAX_LONG]={                    //genero
     "men","women"
 };
-//pre: prenda y gen tienen MAX_LONG componentes 
-//post :si existe una o mas etiqueta en <nombreFichero> perteneciente a PRENDA, prenda toma 
-//ese valor, si se encuentra esta y aparece una etiqueta de genero esta se guarda en gen, y esta,Gen 
-//toma valor true, si no este es false, devuelve true si se ha encotrado una etiqueta que pertenece a 
+//pre: prenda y gen tienen MAX_LONG componentes
+//post :si existe una o mas etiqueta en <nombreFichero> perteneciente a PRENDA, prenda toma
+//ese valor, si se encuentra esta y aparece una etiqueta de genero esta se guarda en gen, y esta,Gen
+//toma valor true, si no este es false, devuelve true si se ha encotrado una etiqueta que pertenece a
 //PRENDA en nombreFichero y false si no se ha encontrado o no se pudo leer
 bool encontrarPrenda(const char nombreFichero[],char prenda[],char gen[],bool& estaGen){
     ifstream f(nombreFichero);//abres el fichero
@@ -56,11 +56,11 @@ bool encontrarPrenda(const char nombreFichero[],char prenda[],char gen[],bool& e
                     }
                 }
             }
-            
-            f>>prendaLeida;            
+
+            f>>prendaLeida;
         }
         f.close();
-        
+
     }
     else{
         cerr<< "no se abre "<< nombreFichero<<endl;
@@ -71,26 +71,26 @@ bool encontrarPrenda(const char nombreFichero[],char prenda[],char gen[],bool& e
 
 // pre:..
 //post: se ha intentado abrir un achivo "datos.txt" con el formato <etiqueta>{"/n" <etiqueta>},
-//si se ha abierto se busca en el si contien una linea igual a algun componente de PRENDAS, 
-//si se encuentra busca una linea igual a algun componente de 
+//si se ha abierto se busca en el si contien una linea igual a algun componente de PRENDAS,
+//si se encuentra busca una linea igual a algun componente de
 int main(){
     char etiquetasFoto[MAX_LONG]="datos.txt";   //archivo donde se guardan las etiquetas
     char prenda [MAX_LONG];
     char gen[MAX_LONG];
     bool estaGen;
     char link [MAX_LONG]=" ";                   // creamos un vector para guardar el link que vamos a crear
-   
+
     if (encontrarPrenda(etiquetasFoto, prenda, gen,estaGen)){
         strcpy(link,INICIO_URL);
-        
+
         if(estaGen){
-            
+
             strcat(link,gen);
             strcat(link,POSTGEN);
         }
         strcat(link,prenda);
     }
-    
+
     ofstream f("url.txt");                      //creas un archivo con el link
 
     if(f.is_open()){
@@ -100,4 +100,3 @@ int main(){
     f.close();
     return 0;
 }
-
